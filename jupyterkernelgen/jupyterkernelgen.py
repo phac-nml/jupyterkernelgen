07e521ec4d3a9d957005d7fe7551428894db320d
Jacob Janzen <jjanzen@cscscience.ca>
2023-06-07 14:37:03 -0500

    add some comments

--- jupyterkernelgen/jupyterkernelgen.py
+++ jupyterkernelgen/jupyterkernelgen.py
@@ -45,6 +45,8 @@ def check_for_conda():
 def get_conda_env():
     try:
         # replace ~ with user's home directory and make it an absolute path
+        # if ~ is not used to refer to the user's home directory, it will
+        # just create an absolute path to the specified location
         conda_env = os.path.abspath(os.path.expanduser(input(f"{colors.HEADER}ENTER PATH TO CONDA ENVIRONMENT{colors.ENDC}: ")))
 
         if os.path.isdir(conda_env + "/conda-meta"): # all conda environments have a /conda-meta directory
@@ -160,18 +162,35 @@ def create_kernel_json(path, kernel_name):
 
 def main():
     try:
+        # Get the name of the kernel
         kernel_name = get_kernel_name()
+
+        # Create a directory for the kernel
         path = create_kernel_dir(kernel_name)
+
+        # Make sure conda or mamba is installed
         conda_exe = check_for_conda()
+
+        # Figure out which conda environment to use
         conda_env = get_conda_env()
+
+        # Ensure that ipykernel is installed
         installed = ipykernel_installed(conda_env)
         if not installed:
+            # Install ipykernel if necessary
             install_ipykernel(conda_exe, conda_env)
+
+            # Exit if ipykernel failed to install
             if not ipykernel_installed(conda_env):
                 print(f"{colors.FAIL}ipykernel installation failed. Exiting...{colors.ENDC}")
                 sys.exit(1)
+
+        # Create script to launch the kernel
         create_kernel_helper_script(path, conda_env)
+
+        # Create json to define the kernel
         create_kernel_json(path, kernel_name)
+
         print(f"{colors.OK}KERNEL INSTALLATION SUCCESS{colors.ENDC}")
     except KeyboardInterrupt:
         print("EXITING...")
